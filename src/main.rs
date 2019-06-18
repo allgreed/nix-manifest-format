7dc9c66496a446ebefb6fcc6a90f918be60e746b
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-06-18 20:42:12 +0200

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,22 @@ use std::fs;
 use std::path::Path;
 
 use dirs::home_dir;
-use rnix::types::*;
 
-// TODO: Add tests
-// TODO: Accumulate prints and print at the end
+mod format_manifest;
+
+use format_manifest::format_manifest;
+
 // TODO: Unwraps -> expects / sth like that
 // TODO: Package via nix
 
-fn main() {
-
+fn main()
+{
     let home_dir = home_dir().unwrap();
     let manifest_relative_location = Path::new(".nix-profile/manifest.nix");
 
-    let content = fs::read_to_string(home_dir.join(manifest_relative_location)).unwrap();
-    let ast = rnix::parse(&content);
+    let manifest_content = fs::read_to_string(home_dir.join(manifest_relative_location)).unwrap();
 
-    let package_list = List::cast(ast.root().inner()).unwrap();
+    let formatted_manifest = format_manifest(manifest_content);
 
-    println!("[");
-
-    for package in package_list.items() {
-        let set = Set::cast(package).unwrap();
-        println!("  {{");
-
-        for entry in set.entries() {
-            if entry.key().node().first_token().unwrap().text() == "meta" {
-                println!("    meta = {{}};");
-            }
-            else {
-                println!("    {}", entry.node());
-            }
-        }
-
-        println!("  }}");
-    }
-
-    println!("]");
+    print!("{}", formatted_manifest);
 }
