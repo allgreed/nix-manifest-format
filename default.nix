d613eb6498ff08fba495f5b1403af52fd72c25af
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-07-02 19:34:33 +0200

    Improve nix packaging

--- default.nix
+++ default.nix
@@ -2,22 +2,22 @@
 
 rustPlatform.buildRustPackage rec {
   name = "nix-manifest-format-${version}";
-  version = "0.3.0"; # ??? how to link this to Cargo.toml ???
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "allgreed";
     repo = "nix-manifest-format";
-    rev = "master"; # TODO: This is crap - start using tags to mark versions
+    rev = "${version}";
     sha256 = "1bm13gwvhbdhljnnsfprfkac0ahgq68xn8wx84xrjnn88pyj2x34";
   };
 
   cargoSha256 = "1i5q0asq4k2hv07f6253yrwbcj22msxv6lagx4fz054sjn1m34j6";
 
-  #meta = with stdenv.lib; {
-  #  description = "A fast line-oriented regex search tool, similar to ag and ack"; # ???
-  #  homepage = https://github.com/BurntSushi/ripgrep;
-  #  license = licenses.unlicense; # ???
-  #  maintainers = [ maintainers.tailhook ]; # ???
-  #  platforms = platforms.all; # ???
-  #};
+  meta = with stdenv.lib; {
+    description = "A utility for formatting and removing metadata from manifest.nix";
+    homepage = https://github.com/allgreed/nix-manifest-format/;
+    license = licenses.mit;
+    maintainers = [ { github = "allgreed"; name = "Olgierd Kasprowicz"; } ];
+    platforms = platforms.all;
+  };
 }
